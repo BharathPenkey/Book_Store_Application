0c7b495654ffa8ce773689166ffffb463747762a
BharathPenkey <penkybharath@gmail.com>
2026-03-27 13:17:58 +0530

    Fronded created routes and pages for home , edit ,create , delete books

--- Frontend/bookstore/src/App.jsx
+++ Frontend/bookstore/src/App.jsx
@@ -1,25 +1,23 @@
-import { useState } from 'react'
+import React from 'react';
+import { Routes,Route} from 'react-router-dom'
+import CreateBook from './Pages/CreateBook';
+import DeleteBook from './Pages/DeleteBook';
+import EditBook from './Pages/EditBook';
+import Home from './Pages/Home';
+import Showbook from './Pages/Showbook';
 
-// import './App.css'
-import './index.css';
 
-function App() {
-  // const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div className="p-10">
-        <h1 className="text-5xl font-bold text-blue-600">
-          Tailwind Working 🚀
-        </h1>
-
-        <button className="mt-5 px-4 py-2 bg-green-500 text-white rounded-lg">
-          Test Button
-        </button>
-      </div>
-    </>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/books/create" element={<CreateBook />}/>
+    <Route path="/books/details/:id" element={<Showbook />}/>
+    <Route path="/books/edit/:id" element={<EditBook />}/>
+    <Route path="/books/delete/:id" element={<DeleteBook />}/>
+    {/* <Route path="" element={}/> */}
+  </Routes>
   )
 }
-
 
 export default App
