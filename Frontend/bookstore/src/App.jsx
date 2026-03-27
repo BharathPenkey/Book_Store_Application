114a2dccacda84ab9301265afdcac990da886b72
BharathPenkey <penkybharath@gmail.com>
2026-03-27 12:11:24 +0530

    Frontend Setup

---
+++ Frontend/bookstore/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+// import './App.css'
+import './index.css';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="p-10">
+        <h1 className="text-5xl font-bold text-blue-600">
+          Tailwind Working 🚀
+        </h1>
+
+        <button className="mt-5 px-4 py-2 bg-green-500 text-white rounded-lg">
+          Test Button
+        </button>
+      </div>
+    </>
+  )
+}
+
+
+export default App
